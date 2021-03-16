b45619b04cc84bc1d8de01ad9370bffe0c1c21e3
Pedro Garcia <drogpe@gmail.com>
2021-03-16 12:03:10 -0300

    Implemented better graphl query test

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,3 +1,2 @@
 /* eslint-disable import/prefer-default-export */
-export { default as test } from './test';
-export const variable = 'varrrrrrrr';
+export { default as getAlerts } from './getAlerts';
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,8 +1,30 @@
 import { buildSchema } from 'graphql';
 
 export default buildSchema(`
+    type Price {
+        price: Float
+        date: String
+    }
+
+    type Product {
+        url: String
+        title: String
+        imageUrl: String
+        currency: String
+        priceHistory: [Price]
+    }
+
+    type Alert {
+        id: ID!
+        ASIN: String
+        targetPrice: Float
+        updatedAt: String
+        createdAt: String
+        productDetails: Product
+    }
+
     type RootQuery {
-        test: String!
+        getAlerts: [Alert]!
     } 
 
     schema {
