65e7d34df9fa4d2aeeee6396a6622dad8d435784
Pedro Garcia <drogpe@gmail.com>
2021-03-16 17:28:19 -0300

    Fixed parameter type

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -64,7 +64,7 @@ export default class UserServices {
             });
     }
 
-    static removeAlert(alertId: ObjectId, userId: ObjectId) {
+    static removeAlert(alertId: ObjectId, userId: string) {
         return User.findById(userId)
             .exec()!
             .then((user) => {
