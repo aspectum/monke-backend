875e772a71dcf498d66ae97d839f3f16da6007ec
Pedro Garcia <drogpe@gmail.com>
2021-03-16 16:05:24 -0300

    Implemented product services

--- src/services/productServices.ts
+++ src/services/productServices.ts
@@ -1,31 +1,36 @@
 import { Product } from '../models/productModel';
 import scrape from '../scraping/scraper';
 import scrapedDataParser from '../helpers/scrapedDataParser';
-import { ProductData, ProductDocument } from '../interfaces';
+import { ProductData } from '../interfaces';
 
 // Defining product services
 export default class ProductServices {
     static findProductByASIN(ASIN: string) {
-        return Product.findById(ASIN).then((product) => {
-            return product;
-        });
-        // return ProductRepository.findProductById(ASIN).then((product) => {
-        //     console.log(product);
-        //     return product;
-        // });
+        return Product.findById(ASIN).exec();
     }
 
-    static createProduct(productData) {
-        return ProductRepository.createProduct(productData).then((product) => {
-            console.log(product);
-            return product;
+    static createProduct(productData: ProductData) {
+        const { ASIN, url, title, imageUrl, currency, price } = productData;
+        const newProd = new Product({
+            _id: ASIN,
+            url,
+            title,
+            imageUrl,
+            currency,
+            price_history: [
+                {
+                    price,
+                },
+            ],
         });
+
+        return newProd.save();
     }
 
-    static scrapeUrl(url) {
-        return scrape(url).then((productData) =>
+    static scrapeUrl(url: string) {
+        return scrape(url).then((productData) => {
             // Processing scraped data
-            scrapedDataParser(productData)
-        );
+            return scrapedDataParser(productData);
+        });
     }
 }
