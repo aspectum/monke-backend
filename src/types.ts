aebe2734790dc87a49ce8657c7a54f75077e538b
Pedro Garcia <drogpe@gmail.com>
2021-03-17 15:01:07 -0300

    Added types to use in auth module

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,19 @@
 import { Schema } from 'mongoose';
+import { Request, Response, NextFunction } from 'express';
+
+// https://stackoverflow.com/questions/42233987/how-to-configure-custom-global-interfaces-d-ts-files-for-typescript
 
 export type ObjectId = Schema.Types.ObjectId;
+
+export type TokenData = {
+    id: string;
+    username: string;
+};
+
+export interface ExpressReq extends Request {
+    authData?: TokenData;
+}
+
+export interface ExpressRes extends Response {}
+
+export interface ExpressNext extends NextFunction {}
