403246b5d3f259252ca94337353c0f80471232a5
Pedro Garcia <drogpe@gmail.com>
2021-03-17 14:59:52 -0300

    Added reference as comment

--- src/graphql/resolvers/getAlerts.ts
+++ src/graphql/resolvers/getAlerts.ts
@@ -1,4 +1,5 @@
 export default () => {
+    // https://stackoverflow.com/questions/49047259/how-to-parse-graphql-request-string-into-an-object
     // import { parse } from 'graphql';
     // console.log(parse(req.body.query).definitions[0].selectionSet.selections[0]);
 
