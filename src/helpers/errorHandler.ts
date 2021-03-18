78d40fa1cf7573346774a37f59a146d4405dba3b
Pedro Garcia <drogpe@gmail.com>
2021-03-18 14:53:10 -0300

    Better handling of graphql originaError

--- src/helpers/errorHandler.ts
+++ src/helpers/errorHandler.ts
@@ -1,19 +1,31 @@
+/* eslint-disable no-console */
 import chalk from 'chalk';
+import { GraphQLError } from 'graphql';
 import { ExpressNext, ExpressReq, ExpressRes } from '../types';
 
 // TODO: better error handling
 // Example
 // if (err.name === 'MongoError' && err.code === 1000 && Object.keys(err.keyPattern).includes('email')) customError.msg(email already exists)
 
-const errorParser = (err: Error) => {
+const errorParser = (err: Error | GraphQLError) => {
     console.log('---------NEW ERROR----------');
     console.log(chalk.bgRed(err.name));
     console.log(chalk.bgRed(err.message));
     console.log(chalk.bgRed(err.stack));
     console.log(Object.getOwnPropertyNames(err));
 
+    let name: string;
+
+    // const t = typeof err
+
+    if ('originalError' in err) {
+        name = err.originalError!.name;
+    } else {
+        name = err.name;
+    }
+
     return {
-        name: err.name,
+        name,
         message: err.message,
     };
 };
