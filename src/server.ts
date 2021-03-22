5728fbc0f310733c0beb101bb6d3929c22b59bed
Pedro Garcia <drogpe@gmail.com>
2021-03-22 14:34:13 -0300

    Using CORS

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
 import auth, { authMiddleware } from './auth';
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/auth', auth);
 app.use(authMiddleware);
