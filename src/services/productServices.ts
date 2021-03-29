0740393a926557654e742bb1de2e63250f5ab708
Pedro Garcia <drogpe@gmail.com>
2021-03-29 14:52:19 -0300

    Added methods to update product prices

--- src/services/productServices.ts
+++ src/services/productServices.ts
@@ -1,5 +1,6 @@
+import chalk from 'chalk';
 import scrapedDataParser from '../helpers/scrapedDataParser';
-import { ProductData } from '../interfaces';
+import { ProductData, ProductDocument } from '../interfaces';
 import { Product } from '../models/productModel';
 import scrape from '../scraping/scraper';
 
@@ -34,5 +35,51 @@ export default class ProductServices {
     // Scraping and parsing url
     static scrapeUrl(url: string) {
         return scrape(url).then(scrapedDataParser);
+    }
+
+    // update priceHistory
+    static updatePriceHistory(product: ProductDocument) {
+        return this.scrapeUrl(product.url).then((data) => {
+            console.log(`Updating price for ${chalk.green(product.title)}`); // TODO: remove
+            const date = new Date();
+            product.priceHistory.push({
+                price: data.price,
+                date,
+            });
+            if (data.price < product.lowestPrice.price) {
+                product.lowestPrice = {
+                    price: data.price,
+                    date,
+                };
+            }
+            return product.save();
+        });
+    }
+
+    // update each product
+    static updateAllProducts() {
+        console.log(chalk.bgCyan('Updating product prices'));
+        const startTime = Date.now();
+        let len: number;
+        return Product.find({})
+            .exec()
+            .then((products) => {
+                len = products.length;
+                return Promise.all(
+                    products.map((prod, index) => {
+                        return new Promise((resolve) => {
+                            setTimeout(() => {
+                                resolve(this.updatePriceHistory(prod));
+                            }, Math.floor(index / 5) * 10000); // Scrape a batch of 5 every 10 seconds
+                        });
+                    })
+                );
+            })
+            .then((values) => {
+                const timeDiff = Math.round((Date.now() - startTime) / 1000);
+                console.log(
+                    chalk.bgGreen(`Updated the prices of ${len} products in ${timeDiff} seconds`)
+                );
+            });
     }
 }
